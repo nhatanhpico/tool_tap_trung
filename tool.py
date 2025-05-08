c27ae8de461be05cbde0abc1860fefe3b03a19f9
Nhat Anh <nmnanh1235@gmail.com>
2025-05-08 09:45:34 +0700

    Update tool.py

--- tool.py
+++ tool.py
@@ -8,10 +8,10 @@ t = (second+minute*60+hour*60*60)*1000
 root = tk.Tk()
 root.attributes("-fullscreen", True)
 
-tk.Label(root, text="Sau này, chỉ có làm thì mới có ăn, nên tập trung đi", font="Helvetica 25 bold", bg="green").pack(anchor = 'w')
-tk.Label(root, text="Mai sau giúp ích cho đất nước!!!", font="Helvetica 25 bold", bg="green").pack(anchor = 'w')
+tk.Label(root, text="Bạn sẽ không thể sử dụng máy tính trong thời gian này.", font="Helvetica 25 bold", bg="green").pack(anchor = 'w')
+tk.Label(root, text="Hãy tập trung làm việc của bạn!", font="Helvetica 25 bold", bg="green").pack(anchor = 'w')
 
-text = tk.Label(root, text="Tắt cái này đi làm con chó", font="Helvetica 25 bold", bg="red")
+text = tk.Label(root, text="Tập trung học", font="Helvetica 25 bold", bg="red")
 text.pack(anchor = 'w')
 
 img = ImageTk.PhotoImage(Image.open("dog.png"))
@@ -20,7 +20,7 @@ windll.user32.BlockInput(True)
 
 
 def unblock():
-	text.configure(text = "Tắt đi không làm con chó nữa =))")
+	text.configure(text = "Tắt")
 	windll.user32.BlockInput(False)
 	tk.Button(root, text ="Triển thôi =))", command = root.destroy, font="Helvetica 15 bold").pack(anchor = 'w')
 
